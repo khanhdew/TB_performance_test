49335bc4c83185dc21b995dd2971c9f8db1e918d
Khanh Nguyen <khanhdeptrai292004@gmail.com>
2024-09-17 10:39:14 +0700

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting script..."
-python3 -m venv venv
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
 source venv/bin/activate
 pip install -r requirements.txt
 read -r -p "1.Run subcriber 2.Run publisher " response
